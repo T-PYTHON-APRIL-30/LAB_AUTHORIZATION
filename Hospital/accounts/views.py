b0b9f0e167ccc374a88ca87fbd4bf2bb354cecec
lena <lenaashayban@gmail.com>
2023-06-10 22:42:09 +0300

    lab30

--- Hospital/accounts/views.py
+++ Hospital/accounts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'accounts/home.html')
+def signin(request):
+    return render(request, 'accounts/signin.html', {})
