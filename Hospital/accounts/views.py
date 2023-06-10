c9e2821080e859770a4e6b08aab2b9eab468ab0c
lena <lenaashayban@gmail.com>
2023-06-10 23:07:50 +0300

    lab30

--- Hospital/accounts/views.py
+++ Hospital/accounts/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 
-def signin(request):
-    return render(request, 'accounts/signin.html', {})
+def signin(request:HttpRequest):
+    msg = None
+    if request == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('clinic_app:home')
+        else:
+            msg = "Incorrect Try Again"
+            return redirect('accounts:signin')
+    return render(request, "accounts/signin.html", {"msg" : msg})
+
+def logout(request:HttpRequest):
+    logout(request)
+    return redirect("clinic_app:home")
